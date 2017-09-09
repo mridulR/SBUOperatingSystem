63f697772da6ce6fce192354baf29bbf9f46a629
Mridul Ranjan <mailtoranjanmridul@gmail.com>
2017-09-08 22:23:31 -0400

    Implemented ls and cat

---
+++ bin/ls/ls.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    DIR *current_working_directory;
+    struct dirent *file;
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    current_working_directory = opendir(cwd);
+    while((file = readdir(current_working_directory)) != NULL)
+    {
+        if ((strcmp(file->d_name, ".") != 0) && (strcmp(file->d_name,"..") != 0)) {
+          fputs(file->d_name, stdout);
+          fputs("\n", stdout);
+        }
+    }
+    closedir(current_working_directory);
+}
