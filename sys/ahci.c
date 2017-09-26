76b157c92bebf1b31fd22178c914f3d8cd774bd2
Rohit Kumar <rohitbcs09@gmail.com>
2017-09-26 12:22:14 -0400

    Basic code- Read Write testing on machine

--- sys/ahci.c
+++ sys/ahci.c
@@ -393,19 +393,16 @@ void init_ahci() {
   for(int i=0; i<NUM_BLOCKS; ++i) {
     writeBuffer[i] = ptr;
     memset(writeBuffer[i], '\0', BLOCK_SIZE);
-    memset(writeBuffer[i], (0x32 + i), BLOCK_SIZE/2);
-    memset(writeBuffer[i]+(BLOCK_SIZE/2), (0x33 + i), (BLOCK_SIZE/2) -1);
+    memset(writeBuffer[i], (0x32 + i), BLOCK_SIZE);
     write(&abar->ports[g_SATA_PORT_INDEX], 0, 0, 1, (WORD *)writeBuffer[i]);
-    ptr = ptr + BLOCK_SIZE/2;
+    ptr = ptr + BLOCK_SIZE;
   }
   
   char *readBuffer[NUM_BLOCKS];
   for(int i = 0; i< NUM_BLOCKS; ++i) {
     readBuffer[i] = NULL;
     read(&abar->ports[g_SATA_PORT_INDEX], 0, 0, 1, (WORD *)readBuffer[i]);
-    kprintf("\n1st RV= %s", readBuffer[i]);
-    //read(&abar->ports[g_SATA_PORT_INDEX], 512, 0, 1, (WORD *)readBuffer[i]);
-    //kprintf("\n2nd RV= %s", readBuffer[i]);
+    kprintf("\n1st Read Value = %s", readBuffer[i]);
   }
 
 }
