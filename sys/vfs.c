b87ade77bea3c0d9bcb1848574a155478c73f9eb
Rohit Kumar <rohitbcs09@gmail.com>
2017-12-04 20:41:04 -0500

    vfs fix

--- sys/vfs.c
+++ sys/vfs.c
@@ -11,43 +11,43 @@
 extern uint64_t KB;
 
 v_file_node* get_root_node() {
-	return root_node; 
+    return root_node; 
 }
 
 
 void print_node_inorder(v_file_node* root) {
-	
-	if (root == NULL) {
-		return;
-	}
+    
+    if (root == NULL) {
+        return;
+    }
 
     kprintf("File Name = %s, File Type isDir = %d, parent = %s , no_of_child = %d\n", 
-		root->v_name, root->is_dir,	root->parent == NULL ? "NULL" : root->parent->v_name, root->no_of_child);	
+        root->v_name, root->is_dir,    root->parent == NULL ? "NULL" : root->parent->v_name, root->no_of_child);    
 
-	for (uint64_t i = 0; i < root->no_of_child; i++) {
-	v_file_node *child = root->v_child[i];
-	if (child->is_dir == false) {
-		kprintf("File Name = %s, File Type isDir = %d, parent = %s, no_of_child = %d\n",
-			 child->v_name, child->is_dir, child->parent == NULL ? "NULL" : child->parent->v_name,
-			 child->no_of_child);
+    for (uint64_t i = 0; i < root->no_of_child; i++) {
+    v_file_node *child = root->v_child[i];
+    if (child->is_dir == false) {
+        kprintf("File Name = %s, File Type isDir = %d, parent = %s, no_of_child = %d\n",
+             child->v_name, child->is_dir, child->parent == NULL ? "NULL" : child->parent->v_name,
+             child->no_of_child);
      } else {
-		 print_node_inorder(child);
-	   }
-	}
+         print_node_inorder(child);
+       }
+    }
 }
 
 v_file_node * build_Node(char * name, v_file_node * parent, int8_t is_dir, uint64_t start, uint64_t end) {
     uint64_t addr = KB + kmalloc(sizeof(v_file_node));
-	v_file_node * curr = (v_file_node *)addr;
-    memset((uint8_t *)curr, '\0', PAGE_SIZE);	
-	memcpy(curr->v_name, name, kstrlen(name));
-	curr->parent = parent;
-	curr->is_dir = is_dir;
+    v_file_node * curr = (v_file_node *)addr;
+    memset((uint8_t *)curr, '\0', PAGE_SIZE);    
+    memcpy(curr->v_name, name, kstrlen(name));
+    curr->parent = parent;
+    curr->is_dir = is_dir;
     curr->no_of_child = 0;
-	curr->start_addr = start;
-    curr->end_addr = end;    	
+    curr->start_addr = start;
+    curr->end_addr = end;        
 
-	return curr;
+    return curr;
 }
 
 
@@ -118,49 +118,49 @@ v_file_node * insert_node_by_name2(char *name, bool is_dir, uint64_t start_addr,
 
 void insert_node_by_name(char *name, bool is_dir, uint64_t start_addr, uint64_t end_addr) {
     uint64_t addr = KB + kmalloc(sizeof(char) * kstrlen(name));
-	char *path = (char *) addr;
-    memset((uint8_t *)path, '\0', PAGE_SIZE);	
-	memcpy(path, name, kstrlen(name));
+    char *path = (char *) addr;
+    memset((uint8_t *)path, '\0', PAGE_SIZE);    
+    memcpy(path, name, kstrlen(name));
 
-	char *remaining = path;
-	char *temp = kstr_tok(path, '/', &remaining);
+    char *remaining = path;
+    char *temp = kstr_tok(path, '/', &remaining);
     
-	int done = 0;
+    int done = 0;
     while ((done == 0) && (temp != NULL) && (*temp != '\0')) {
-		int flag_child_of_directory = 0;
-		v_file_node * trav_node = tarfs_mount_node;
-		for (int index = 0; index < trav_node->no_of_child; index++) {
-			if (kstrcmp(temp, trav_node->v_child[index]->v_name) == 0) {
-				flag_child_of_directory = 1;
+        int flag_child_of_directory = 0;
+        v_file_node * trav_node = tarfs_mount_node;
+        for (int index = 0; index < trav_node->no_of_child; index++) {
+            if (kstrcmp(temp, trav_node->v_child[index]->v_name) == 0) {
+                flag_child_of_directory = 1;
                 temp = kstr_tok(remaining, '/', &remaining);
                 trav_node = trav_node->v_child[index];
                 if (temp == NULL || (*temp == '\0')) {
                     done = 1;
                 }
-				break;
-			}
-			trav_node = (v_file_node *)trav_node->v_child[index];
-		}
+                break;
+            }
+            trav_node = (v_file_node *)trav_node->v_child[index];
+        }
 
         if (done == 0) {    
-		if(flag_child_of_directory == 0) {
+        if(flag_child_of_directory == 0) {
             trav_node = tarfs_mount_node;
         }
-		while ((temp != NULL) &&  (*temp != '\0')) {
-			v_file_node * curr_node = build_Node(temp, trav_node, 1, 0, 0);
+        while ((temp != NULL) &&  (*temp != '\0')) {
+            v_file_node * curr_node = build_Node(temp, trav_node, 1, 0, 0);
             trav_node->v_child[trav_node->no_of_child] = curr_node;
             trav_node->no_of_child = trav_node->no_of_child + 1;
             trav_node = curr_node; 
             temp = kstr_tok(remaining, '/', &remaining);
         }
-		trav_node->start_addr = start_addr;
-		trav_node->end_addr = end_addr;
+        trav_node->start_addr = start_addr;
+        trav_node->end_addr = end_addr;
         trav_node->is_dir = is_dir;
         done = 1;
     }
-	temp = kstr_tok(remaining, '/', &remaining);
-	}
-	kfree((uint64_t)path);
+    temp = kstr_tok(remaining, '/', &remaining);
+    }
+    kfree((uint64_t)path);
 }
 
 
@@ -176,111 +176,110 @@ int32_t atoi(char *ch) {
 
 
 int32_t oct_to_dec(int32_t num) {
-	int decimal = 0;
+    int decimal = 0;
     int base = 1;
-	while (num != 0) {
-		decimal +=  (num % 10) * base;
-		base = base * 8;
+    while (num != 0) {
+        decimal +=  (num % 10) * base;
+        base = base * 8;
         num = num / 10;
-	}
-	return decimal;
+    }
+    return decimal;
 }
 
 
 void* init_tarfs() {
     root_node = build_Node("/", NULL, true, 0, 0);
-	tarfs_mount_node = build_Node("rootfs", root_node, true, 0, 0);
+    tarfs_mount_node = build_Node("rootfs", root_node, true, 0, 0);
     root_node->no_of_child += 1;
-	root_node->v_child[0] = tarfs_mount_node;
+    root_node->v_child[0] = tarfs_mount_node;
 
-	posix_header_ustar* start = (posix_header_ustar*) & _binary_tarfs_start;
+    posix_header_ustar* start = (posix_header_ustar*) & _binary_tarfs_start;
 
-	int file_size = 0;
-	while(start < (posix_header_ustar*) & _binary_tarfs_end) {
+    int file_size = 0;
+    while(start < (posix_header_ustar*) & _binary_tarfs_end) {
 
         if (start->name == NULL || kstrlen(start->name) < 3) {
             start = start + 1;
             continue;
         }
 
-		file_size = oct_to_dec(atoi(start->size));
+        file_size = oct_to_dec(atoi(start->size));
         //if(kstrcmp(start->name, "-\n") != 0){
         //    kprintf("\nFile Name - %s,  %d\n", start->name, file_size);
         // }
-		if (kstrcmp(start->typeflag, "5") == 0) {
-			insert_node_by_name2(start->name, true, 0, 0, tarfs_mount_node);
-		} else {
-			insert_node_by_name2(start->name, false, (uint64_t) (start + 1),
-				 (uint64_t) ((void *)start + 512 + file_size), tarfs_mount_node);
+        if (kstrcmp(start->typeflag, "5") == 0) {
+            insert_node_by_name2(start->name, true, 0, 0, tarfs_mount_node);
+        } else {
+            insert_node_by_name2(start->name, false, (uint64_t) (start + 1),
+                 (uint64_t) ((void *)start + 512 + file_size), tarfs_mount_node);
         }
         
-		if (file_size > 0) {
+        if (file_size > 0) {
             uint64_t offset = (file_size / sizeof(posix_header_ustar));
             if(file_size % sizeof(start) == 0) {
                 offset += 1;
             }
-			start = start + offset + 1; // * sizeof(posix_header_ustar))+ 2;
+            start = start + offset + 1; // * sizeof(posix_header_ustar))+ 2;
         } else {
-			start = start + 1;
+            start = start + 1;
         }
-    } 	
-	return (void *)root_node ;
+    }     
+    return (void *)root_node ;
 }
 
 
 v_file_node* search_file(char* dir_path, v_file_node * start_node) {
     if (start_node == NULL) {
-		return NULL;
-	}
+        return NULL;
+    }
 
-	if (dir_path == NULL || (*dir_path == '\0')) {
-		return NULL;
-	}
+    if (dir_path == NULL || (*dir_path == '\0')) {
+        return NULL;
+    }
 
     uint64_t addr = KB + kmalloc(sizeof(char) * kstrlen(dir_path));
-	char *path = (char *) addr;
+    char *path = (char *) addr;
     memset((uint8_t *)path, '\0', PAGE_SIZE);   
     memcpy(path, dir_path, kstrlen(dir_path));
 
     char *remaining = path;
     char *temp = kstr_tok(path, '/', &remaining);
 
-	if (kstrcmp(temp, start_node->v_name) != 0) {
-		kfree((uint64_t)path);
+    if (kstrcmp(temp, start_node->v_name) != 0) {
+        kfree((uint64_t)path);
         return NULL;
-    } else {
-		if (remaining == NULL || *remaining == '\0') {
-			return start_node;
-		}
-	}
-
-	if (remaining == NULL || *remaining == '\0') {
-		kfree((uint64_t)path);
-		return NULL;
-	}
-
-	temp = kstr_tok(remaining, '/', &remaining);
-
-	for (int index = 0; index < start_node->no_of_child; index++) {
-        if (kstrcmp(temp, start_node->v_child[index]->v_name) == 0) {
-			if (remaining == NULL || *remaining == '\0') {
-				return start_node->v_child[index];
-			}
-			temp = kstr_tok(remaining, '/', &remaining);
-			v_file_node * child = start_node->v_child[index];
-			for (int ch = 0; ch < child->no_of_child; ch++) {
-				v_file_node * rslt = search_file(temp, child->v_child[ch]);
-				if (rslt != NULL) {
-					return rslt;
-				}
-			}
-            return NULL;
-        } else {
-			return NULL;
+    } 
+    else {
+        if (remaining == NULL || *remaining == '\0') {
+            return start_node;
         }
-	}
-	kfree((uint64_t)path);
-	return NULL;
+    }
+
+    if (remaining == NULL || *remaining == '\0') {
+        kfree((uint64_t)path);
+        return NULL;
+    }
+
+    temp = kstr_tok(remaining, '/', &remaining);
+
+    for (int index = 0; index < start_node->no_of_child; index++) {
+        if (kstrcmp(temp, start_node->v_child[index]->v_name) == 0) {
+            if (remaining == NULL || *remaining == '\0') {
+                return start_node->v_child[index];
+            }
+            temp = kstr_tok(remaining, '/', &remaining);
+            v_file_node * child = start_node->v_child[index];
+            for (int ch = 0; ch < child->no_of_child; ch++) {
+                v_file_node * rslt = search_file(temp, child->v_child[ch]);
+                if (rslt != NULL) {
+                    return rslt;
+                }
+            }
+            return NULL;
+        } 
+    }
+    kfree((uint64_t)path);
+    return NULL;
 }
 
 
