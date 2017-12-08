e0f456425b9e36c5fc594843ad1e9c5abf6b4bb3
Rohit Kumar <rohitbcs09@gmail.com>
2017-12-08 00:12:55 -0500

    more libs

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "syscall.h"
 
 int main(int argc, char *argv[], char *envp[]) {
     
     char buf[1024];
-    memset(buf, '\0', 1024); 
     while(1) {
       puts("\nsbush~>");
       gets(buf);
       puts(buf);
+      memset(buf, '\0', strlen(buf)); 
     }
 
     mmap((uint64_t *)0xFFF, 0x10, 0x20, 0x30, 0x40, 0x50);
---
+++ include/string.h
@@ -0,0 +1,12 @@
+#ifndef _STRING_H
+#define _STRING_h
+
+#define NULL ((void*)0)
+
+int strcmp(const char * s1, const char * s2);
+
+int strlen(const char *str);
+
+char *strchr(char *s, char c);
+
+#endif
